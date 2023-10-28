bcbf65278c0847bf49d23035a032ac6524e2447e
YuetAu <sam07@live.hk>
2023-10-29 01:11:08 +0800

    notistack

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { SnackbarProvider } from 'notistack'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <SnackbarProvider 
+        autoHideDuration={1000}
+      >
       <Component {...pageProps} />
+      </SnackbarProvider>
     </ChakraProvider>
   )
 }
